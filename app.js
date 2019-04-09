be1409bae4798a1607d5b43547e1b32c0caa896a
Daniel Barros <barros.daniel@outlook.com>
2019-04-10 07:30:47 +0800

    feature: Add game variables to script

---
+++ app.js
@@ -0,0 +1,23 @@
+// Players must guess a number between a min and max
+// PLayers get a certain number of guesses
+// Notify players of remaining guesses
+// Notify player of the correct answer if lose
+// Let player choose to play again
+
+// Game values
+let min = 1,
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
+
+// UI elements
+const game = document.querySelector("#game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
+  message = document.querySelector(".message");
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
